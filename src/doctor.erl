2c159a2654fd134bd9d55ed0d6b9ff9033b18162
varvelworld <varvelworld@gmail.com>
2015-02-22 23:34:41 +0800

    修改docotr

--- src/doctor.erl
+++ src/doctor.erl
@@ -16,18 +16,29 @@
 loop(F, A) ->
   process_flag(trap_exit, true),
   receive
-    new ->
-      io:format("Creating and monitoring process.~n"),
-      register(A, spawn_link(F)),
+    start ->
+      io:format("Start and monitoring process.~n"),
+      Found = lists:member(A, registered()) andalso erlang:is_process_alive(whereis(A)),
+      if
+        Found ->
+          io:format("Reattaching to running process ~p.~n", [A]),
+          link(whereis(A));
+        true ->
+          io:format("Creating and monitoring process, attaching to atom ~p.~n", [A]),
+          register(A, spawn_link(F))
+      end,
       loop(F, A);
+    die ->
+      io:format("I'm died~n"),
+      exit({die, at, time, erlang:time()});
     {'EXIT', From, Reason} ->
       io:format("The process ~p died with reson ~p~n", [From, Reason]),
       io:format(" Restarting~n."),
-      self() ! new,
+      self() ! start,
       loop(F, A)
   end.
 
 start(F, A) ->
   D = spawn(doctor, loop, [F, A]),
-  D ! new,
+  D ! start,
   D.
